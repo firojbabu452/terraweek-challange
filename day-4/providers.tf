e89e6890e14865976489858937e18abb8ccb87e0
Firoj Babu <firojbabu55@gmail.com>
2026-07-15 10:11:49 +0000

    day 4 challange complete

---
+++ day-4/providers.tf
@@ -0,0 +1,12 @@
+
+provider "aws" {
+  region = var.aws_region
+
+  default_tags {
+    tags = {
+      Project   = "terraweek-2026"
+      ManagedBy = "terraform"
+      Purpose   = "tf-state-backend"
+    }
+  }
+}
